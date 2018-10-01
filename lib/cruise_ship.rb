94da31d6660d09555d4812ceb2deb58b7a2a74ed
Virginia Dooley <vcdooley@gmail.com>
2018-10-01 11:45:17 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,12 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = "" #creates a new variable
+    passengers.each do |suite, name| #iterates over all key value pairs, 
+      if suite == :suite_a && name.start_with?("A") #selects the name that meet the suite condition
+    winner = name #puts the result
+    end
+end
+ 
+winner  
 end
